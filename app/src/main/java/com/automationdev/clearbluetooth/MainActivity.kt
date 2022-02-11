614cd512df63fbda4ca088b8fdf9439325829794
Sami Väntti <sami.vantti@ouraring.com>
2022-02-11 17:10:40 +0200

    Add list for connected and disconnected devices

--- app/src/main/java/com/automationdev/clearbluetooth/MainActivity.kt
+++ app/src/main/java/com/automationdev/clearbluetooth/MainActivity.kt
@@ -8,7 +8,9 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.os.Build
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import java.lang.Exception
@@ -16,9 +18,18 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
     private val TAG: String = MainActivity::class.java.getSimpleName()
 
+    // Elements
     private lateinit var unpairButton: Button
     private lateinit var statusText: TextView
+    private lateinit var pairedDevicesListView: ListView
+    private lateinit var unpairedDevicesListView: ListView
     private lateinit var pairedDevices: Set<BluetoothDevice>
+
+    // Variables
+    private var pairedDevicesList: MutableList<String> = mutableListOf()
+    private var unpairedDevicesList: MutableList<String> = mutableListOf()
+    private lateinit var arrayAdapterPaired: ArrayAdapter<String>
+    private lateinit var arrayAdapterUnpaired: ArrayAdapter<String>
 
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
@@ -42,6 +53,18 @@ class MainActivity : AppCompatActivity() {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         pairedDevices = bluetoothManager.getAdapter().bondedDevices
 
+        // Get names for paired devices
+        for (device in pairedDevices) {
+            pairedDevicesList.add(device.address)
+        }
+
+        // Set adapters
+        pairedDevicesListView = findViewById(R.id.pairedDevices)
+        arrayAdapterPaired = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, this.pairedDevicesList)
+        pairedDevicesListView.adapter = arrayAdapterPaired
+
+
+
         // Change text
         statusText = findViewById(R.id.bl_devices_text)
         statusText.setText("There is currently ${pairedDevices.size} paired bluetooth devices.")
@@ -54,6 +77,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun unpairAll(pairedDevices: Set<BluetoothDevice>) {
+        // empty list
+        this.unpairedDevicesList = mutableListOf()
+
+        // Set unpair button
         this.unpairButton.setEnabled(false)
         this.unpairButton.setText("Please wait...")
 
@@ -63,11 +90,18 @@ class MainActivity : AppCompatActivity() {
                 try {
                     device::class.java.getMethod("removeBond").invoke(device)
                     unpairCount++
+                    this.unpairedDevicesList.add(device.address)
+                    this.pairedDevicesList.remove(device.address)
 
                 } catch (e: Exception) {
                     Log.w(TAG,"Failed to un-pair device: ${device.address}")
                 }
             }
+            // Show unpaired devices on the list
+            this.arrayAdapterPaired.notifyDataSetChanged()
+            unpairedDevicesListView = findViewById(R.id.unpairedDevices)
+            arrayAdapterUnpaired = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, this.unpairedDevicesList)
+            unpairedDevicesListView.adapter = arrayAdapterUnpaired
         }
 
         statusText.text = String.format("${unpairCount} of ${pairedDevices.size} devices un-paired.")
